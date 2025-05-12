eb180904197decb3c0bd74744fcea35db71f8db9
Ramon Souza <ramonsouzasil2004@gmail.com>
2025-05-12 07:40:37 -0300

    Subindo Musicas
    - Musicas Menu, fase, derrota e vitória
    - Texto piscando no menu
    - Sistema de vida alterado

--- Assets/Scripts/ChamaCenaJogo.cs
+++ Assets/Scripts/ChamaCenaJogo.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class ChamaCenaJogo : MonoBehaviour
 {
     public string gameSceneName = "Game"; // Altere para o nome da sua cena principal
 
     [SerializeField] private GameObject scenerLoadUI;
-    [SerializeField] private GameObject texto;
+    [SerializeField] private TextMeshProUGUI texto;
     [SerializeField] private GameObject texto2;
     [SerializeField] private Slider loadingBar;
-
+    private Coroutine piscarTexto;
+    private void Start()
+    {
+        if (texto != null)
+        {
+            piscarTexto = StartCoroutine(PiscarTexto());
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -33,7 +41,7 @@ public class ChamaCenaJogo : MonoBehaviour
         // esconde o texto "pressione espaço"
         if (texto != null)
         {
-            texto.SetActive(false);
+            texto.gameObject.SetActive(false);
             texto2.SetActive(true);
         }
 
@@ -61,4 +69,29 @@ public class ChamaCenaJogo : MonoBehaviour
 
         asyncLoad.allowSceneActivation = true;
     }
+
+    IEnumerator PiscarTexto()
+    {
+        Color cor = texto.color;
+
+        while (true)
+        {
+            while (cor.a > 0f)
+            {
+                cor.a -= Time.deltaTime * 2f;
+                texto.color = cor;
+                
+                yield return null;
+            }
+
+            while (cor.a < 1f)
+            {
+                cor.a += Time.deltaTime * 2f;
+                texto.color = cor;
+                yield return null;
+            }
+        }
+    }
+
+
 }
